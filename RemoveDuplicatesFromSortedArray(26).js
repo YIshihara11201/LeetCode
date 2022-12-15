e43aeb61f8ddb96ace5b27c4dbea34f4ac090d00
YIshihara11201 <98417271+YIshihara11201@users.noreply.github.com>
2022-12-15 14:06:50 -0800

    Create RemoveDuplicatesFromSortedArray(26).js

---
+++ RemoveDuplicatesFromSortedArray(26).js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates = (nums) => {
+   let i=0;
+    for(let j=0; j<nums.length; j++){
+        if(nums[j]!=nums[i]){
+            nums[++i] = nums[j];
+        }
+    }
+    return ++i;
+};
