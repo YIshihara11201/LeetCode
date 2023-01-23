f53506028cd781241f7c245548933896fff300e3
YIshihara11201 <98417271+YIshihara11201@users.noreply.github.com>
2023-01-22 23:00:03 -0800

    Create ReverseLinkedList(206).swift

---
+++ ReverseLinkedList(206).swift
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        var h = head
+        var res: ListNode? = nil
+        while h != nil {
+            var tmp = h?.next
+            h?.next = res
+            res = h
+            h = tmp
+        }
+        
+        return res
+    }
+}
