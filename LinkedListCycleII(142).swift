bb034cff4381f962d47843831802cce6b71f8c23
YIshihara11201 <98417271+YIshihara11201@users.noreply.github.com>
2023-01-26 21:03:30 -0800

    Create LinkedListCycleII(142).swift

---
+++ LinkedListCycleII(142).swift
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func detectCycle(_ head: ListNode?) -> ListNode? {
+        var slow = head
+        var fast = head
+        while fast != nil && fast?.next != nil {
+            slow = slow!.next
+            fast = fast!.next!.next
+            if slow === fast { 
+                var h = head
+                while h !== slow {
+                    h = h!.next
+                    slow = slow!.next
+                }
+                return h
+            }
+        }
+    
+        return nil
+    }
+}
